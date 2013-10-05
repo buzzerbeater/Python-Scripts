a975e82c472c0c7ab3e88620ffe7a6d1eb19b684
Xing Fan <Alex.Fanxing@gmail.com>
2013-10-05 12:57:38 +0800

    Update split a pdf.py

--- split a pdf.py
+++ split a pdf.py
@@ -1,6 +1,5 @@
 from pyPdf import PdfFileWriter,PdfFileReader
 pdf=PdfFileReader(file('aa.pdf','rb'))
-pagenum=pdf.numPages
 pagelist=[[1,2],[3,4,5],[6,7,8,9,10],[11,12,13,14,15,16,17,18,19,20]]
 for i in range(len(pagelist)):
 	out=PdfFileWriter()
